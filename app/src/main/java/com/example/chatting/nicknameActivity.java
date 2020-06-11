48000c0dc18e164a6a23ea440b5f0bd7ec68d368
DD1030 <62937322+DD1030@users.noreply.github.com>
2020-06-11 22:11:12 +0900

    디자인 구림, 새로 만들 예정

---
+++ app/src/main/java/com/example/chatting/nicknameActivity.java
@@ -0,0 +1,45 @@
+package com.example.chatting;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class nicknameActivity extends AppCompatActivity {
+
+    private EditText et_user;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setTitle("로그인");
+        setContentView(R.layout.chatting_nickname);
+
+        et_user = (EditText) findViewById(R.id.et_user);
+        btn_login = (Button) findViewById(R.id.btn_chat_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str_name = et_user.getText().toString();;
+
+                if ("".equals(str_name)){
+                    Toast.makeText(nicknameActivity.this, "닉네임을 입력해주세요!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(nicknameActivity.this, MainActivity.class);
+                    intent.putExtra("name", str_name);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+}
